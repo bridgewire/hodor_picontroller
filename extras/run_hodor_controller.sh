9388b9d2ce6442c95082bd40d71ee273aad2b6c9
Hodor Picontroller <hodor@example.com>
2017-05-06 23:46:34 +0000

    capture updated run script

--- extras/run_hodor_controller.sh
+++ extras/run_hodor_controller.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
+controllerlogfile="/home/pi/log/run_hodor_controller_"`date +%Y%m%d_%H%M%S`"_$$.log"
+slackerlogfile="/home/pi/log/run_hodor_slacker_"`date +%Y%m%d_%H%M%S`"_$$.log"
 cd /home/pi/repos/hodor_picontroller
-python hodor_controller/watcher.py
+export PYTHONUNBUFFERED=1
+touch ${controllerlogfile}
+touch ${slackerlogfile}
+python hodor_controller/watcher.py --root /home/pi >> ${controllerlogfile} 2>&1 &
+python hodor_slackbot/hodor_slacker.py --root /home/pi >> ${slackerlogfile} 2>&1 &
+exit 0
