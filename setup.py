52f98c8981989c3d1bf47f3da798a22ba38235e1
Ralph Santos <ralfarama@yahoo.com>
2018-02-24 14:13:18 -0800

    paybook unify and extend

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(name='hodor_picontroller',
     version='1.1',
     description='Door Controller',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'hodor_watcher=hodor_controller.watcher:main',
+            'hodor_slacker=hodor_slackbot.hodor_slacker:main'
+        ]
+    },
     test_suite="unit_test_suite")
