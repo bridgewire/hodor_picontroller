cd5c0648aa53b8ffb0d2507d553bb1b117d2b7f7
Ralph Santos <ralfarama@yahoo.com>
2020-04-19 21:46:38 +0100

    moving to systemd

--- deploy_ansible/scripts/run_hodor_watcher.sh
+++ deploy_ansible/scripts/run_hodor_watcher.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-controllerlogfile="{{ slackbot_log_dir }}/run_hodor_controller_"`date +%Y%m%d_%H%M%S`"_$$.log"
+watcherlogfile="{{ slackbot_log_dir }}/run_hodor_watcher.log"
 
 export PYTHONUNBUFFERED=1
-touch ${controllerlogfile}
-rm -f /var/run/hodor_controller.pid
-cd {{ controller_run_dir }} && \
-python {{ controller_install_dir }}/hodor_controller/watcher.py \
---root {{ controller_run_dir }} >> ${controllerlogfile} 2>&1
+# touch ${watcherlogfile}
+# rm -f /var/run/hodor_controller.pid
+cd {{ watcher_run_dir }} && \
+python {{ watcher_install_dir }}/hodor_controller/watcher.py \
+--root {{ watcher_run_dir }} >> ${watcherlogfile} 2>&1
