86bd3ea55bb923aa15e880915099e4f54fafaffe
Ralph Santos <ralfarama@yahoo.com>
2020-04-19 16:54:59 -0700

    start ansible tasks for systemd setup

--- deploy_ansible/scripts/run_hodor_slacker.sh
+++ deploy_ansible/scripts/run_hodor_slacker.sh
@@ -3,8 +3,7 @@
 slackerlogfile="{{ slacker_log_dir }}/run_hodor_slacker.log"
 
 export PYTHONUNBUFFERED=1
-# touch ${slackerlogfile}
-# rm -f /var/run/hodor_slacker.pid
+
 cd {{ slacker_run_dir }} && \
 python {{ slacker_install_dir }}/hodor_slackbot/hodor_slacker.py \
 --root {{ slacker_run_dir }} >> ${slackerlogfile} 2>&1
--- deploy_ansible/scripts/run_hodor_watcher.sh
+++ deploy_ansible/scripts/run_hodor_watcher.sh
@@ -3,8 +3,7 @@
 watcherlogfile="{{ watcher_log_dir }}/run_hodor_watcher.log"
 
 export PYTHONUNBUFFERED=1
-# touch ${watcherlogfile}
-# rm -f /var/run/hodor_controller.pid
+
 cd {{ watcher_run_dir }} && \
 python {{ watcher_install_dir }}/hodor_controller/watcher.py \
 --root {{ watcher_run_dir }} --dev /dev/ttyUSB0 >> ${watcherlogfile} 2>&1
