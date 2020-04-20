1f5cbd37fb32436b100dedf9ec74f384c33d7dc3
Ralph Santos <ralfarama@yahoo.com>
2020-04-19 19:30:53 -0700

    ansible playbook updates

--- deploy_ansible/scripts/run_hodor_slacker.sh
+++ deploy_ansible/scripts/run_hodor_slacker.sh
@@ -5,5 +5,5 @@ slackerlogfile="{{ slacker_log_dir }}/run_hodor_slacker.log"
 export PYTHONUNBUFFERED=1
 
 cd {{ slacker_run_dir }} && \
-python {{ slacker_install_dir }}/hodor_slackbot/hodor_slacker.py \
+python3 {{ slacker_install_dir }}/hodor_slackbot/hodor_slacker.py \
 --root {{ slacker_run_dir }} >> ${slackerlogfile} 2>&1
--- deploy_ansible/scripts/run_hodor_watcher.sh
+++ deploy_ansible/scripts/run_hodor_watcher.sh
@@ -5,5 +5,5 @@ watcherlogfile="{{ watcher_log_dir }}/run_hodor_watcher.log"
 export PYTHONUNBUFFERED=1
 
 cd {{ watcher_run_dir }} && \
-python {{ watcher_install_dir }}/hodor_controller/watcher.py \
+python3 {{ watcher_install_dir }}/hodor_controller/watcher.py \
 --root {{ watcher_run_dir }} --dev /dev/ttyUSB0 >> ${watcherlogfile} 2>&1
